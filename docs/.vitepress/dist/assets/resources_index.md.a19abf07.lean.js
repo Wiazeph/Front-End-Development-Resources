e5d74274adfc4a057c649c5a3b91a51fb6aec2ab
osmandlsmn <dlsmnosman@gmail.com>
2023-03-14 22:43:07 +0300

    feat: add vercel support

---
+++ docs/.vitepress/dist/assets/resources_index.md.a19abf07.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as t,N as a}from"./chunks/framework.3d3bd3d9.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"resources/index.md"}'),o={name:"resources/index.md"},l=a("",4),s=[l];function i(n,c,_,f,h,p){return r(),t("div",null,s)}const u=e(o,[["render",i]]);export{m as __pageData,u as default};
